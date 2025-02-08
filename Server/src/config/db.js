be9adf165733e7418b1714c11ec48a99cdf490b4
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-08 18:57:36 +0530

    Database connection with mongo.

---
+++ Server/src/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../utils/constants.js";
+import logger from "../logger.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+
+        const MONGO_URI = `${process.env.MONGO_URI}/ ${DB_NAME}`;
+        const connectionInstance = await mongoose.connect(MONGO_URI);
+
+        logger.info(`MONGODB connected with : ${connectionInstance.host}`);
+    } catch (error) {
+        logger.error(`MONGODB connection failed due to : ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
