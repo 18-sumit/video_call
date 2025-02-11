1863141a62c2f78c259f9d3635281214b411e849
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-12 00:30:57 +0530

    user routes

--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { registeredUser, loginUser } from "../controllers/user.controller.js";
 
+const UserRouter = Router();
 
-const router = Router();
+// router.post('/register').post(registeredUser);
+// router.post('/login', loginUser)
 
-router.post('/register', registeredUser);
-router.post('/login', loginUser)
+UserRouter.post('/register', registeredUser)
+
+export default UserRouter;
