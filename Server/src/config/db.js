0b2c1921daa1e8d16565e15147fd2a0bd9af4802
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-16 23:22:58 +0530

    basic error handling

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -2,14 +2,22 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../utils/constants.js";
 import logger from "../logger.js";
 import dotenv from "dotenv";
+import { ApiError } from "../utils/ApiError.js"
 
 dotenv.config();
+
+if (!DB_NAME) {
+    throw new ApiError(
+        400,
+        "Please define the DB_NAME in constant.js file"
+    );
+}
 
 const connectDB = async () => {
     try {
 
-        const MONGO_URI = `${process.env.MONGO_URI}/${DB_NAME}`;
-        const connectionInstance = await mongoose.connect(MONGO_URI);
+        const MONGODB_URI = `${process.env.MONGODB_URI}/${DB_NAME}`;
+        const connectionInstance = await mongoose.connect(MONGODB_URI);
 
         logger.info(`MONGODB connected with HOST: ${connectionInstance.connection.host}`);
 
