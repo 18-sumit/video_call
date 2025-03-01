2542cc99a0662bd407bb227266badd188b5597bb
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-02 01:19:06 +0530

    middleware to configure Owner of room

---
+++ Server/src/middleware/admin.middleware.js
@@ -0,0 +1,24 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Room } from "../models/room.models.js";
+
+export const checkRoomAdmin = asyncHandler(async (req, res) => {
+    const { roomId } = req.params;
+    const userId = req.user.id
+
+    const room = await Room.findById(roomId);
+
+    if (!room) {
+        throw new ApiError(
+            404,
+            "Room not found"
+        )
+    }
+
+    if (room.createdBy[0].toString() !== req.userId.toString()) {
+        throw new ApiError(403, "Only the room owner can perform this action")
+    }
+
+    next(); // continue if the user is owner
+})
