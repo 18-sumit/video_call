727eb2e308e37e5d37337542ea42751c1262bc3f
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-01 21:14:19 +0530

    room model done

---
+++ Server/src/models/room.models.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const RoomSchema = new mongoose.Schema(
+    {
+        roomName: {
+            type: String,
+            required: [true, "Room Name is required"],
+            trim: true,
+            minLength: 2,
+            maxLength: 50
+        },
+        roomId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        participants: [ // array cuz their will be multiple participants
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+
+    },
+    { timestamps: true }
+)
+
+export const Room = mongoose.models.Room || mongoose.model("Room", RoomSchema)
