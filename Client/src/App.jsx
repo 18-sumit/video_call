610be21ef3bb5423bb6f19a735c9ded1d704e7a0
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-01 12:08:27 +0530

    main files intialized

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <body>
+        <h1 class="text-3xl font-bold underline text-amber-200">
+          Hello world!
+        </h1>
+      </body>
+    </>
+  )
+}
+
+export default App
