8028952e4c29816995835ecb7d1863cdd82b0746
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-03 00:08:58 +0530

    updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LobbyScreen from './Screens/Lobby.jsx'
+
+
+
 function App() {
 
   return (
-    <>
-      <body>
-        <h1 class="text-3xl font-bold underline text-amber-200">
-          Hello world!
-        </h1>
-      </body>
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<LobbyScreen />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ Client/src/Screens/Lobby.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const LobbyScreen = () => {
+    return (
+        <div>
+            <h1>Lobby Screen</h1>
+        </div>
+    )
+}
+
+
+export default LobbyScreen;
