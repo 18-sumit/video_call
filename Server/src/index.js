10fb3a549203203890483449081a0c08bba0b8b1
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-02 21:34:30 +0530

    Sockets integration

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,22 +2,32 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import logger from "./logger.js";
+import { createServer } from "http";
+import { initializeSockets } from "./sockets.js"; // setup of websocket
 
 dotenv.config({
     path: "/.env"
 })
 
+
+const PORT = process.env.PORT || 8000;
+
+// Create HTTP server (Required for WebSockets)
+const server = createServer(app);
+
+
+
+
 try {
     await connectDB();
 
-    app.on("error", (error) => {
-        logger.error("Error", error);
-    });
+    // Initialize WebSockets (Pass server)
+    initializeSockets(server);
 
-    app.listen(process.env.PORT || 8000, () => {
-        logger.info(`Server is running on PORT : ${process.env.PORT}`);
+    server.listen(PORT, () => {
+        logger.info(`Server is running on PORT : ${PORT}`)
     });
 
 } catch (error) {
-    logger.error(`MONGODB connection failed due to : ${error.message}`);
+    logger.error(`MONGODB connection failed  : ${error.message}`);
 }
