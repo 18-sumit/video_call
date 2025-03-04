d4b18309bc1dc6f8327dd879139fbecefd76671d
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-04 12:54:10 +0530

    start call function done

---
+++ Server/src/controllers/call.controller.js
@@ -0,0 +1,49 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Call } from "../models/calls.models.js";
+
+
+
+
+
+const initiateCall = asyncHandler(async (req, res) => {
+
+    const { roomId, callerId } = req.body;
+    if (!roomId || !callerId) {
+        throw new ApiError(404, "RoomId and CallerId not found");
+    }
+
+    req.app.get("io").to(roomId).emit("callstarted", { callerId });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            roomId,
+            "call started successfully"
+        ))
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {
+    initiateCall,
+
+}
