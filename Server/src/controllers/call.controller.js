5c854d868f2af0e072979869db4988b1d30495f8
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-10 14:21:20 +0530

    updated

--- Server/src/controllers/call.controller.js
+++ Server/src/controllers/call.controller.js
@@ -9,12 +9,12 @@ import { Call } from "../models/calls.models.js";
 
 const initiateCall = asyncHandler(async (req, res) => {
 
-    const { roomId, callerId } = req.body;
-    if (!roomId || !callerId) {
+    const { roomId, callId } = req.body;
+    if (!roomId || !callId) {
         throw new ApiError(404, "RoomId and CallerId not found");
     }
 
-    req.app.get("io").to(roomId).emit("callstarted", { callerId });
+    req.app.get("io").to(roomId).emit("callstarted", { callId });
 
     return res
         .status(200)
@@ -29,8 +29,20 @@ const initiateCall = asyncHandler(async (req, res) => {
 
 
 const endCall = asyncHandler(async (req, res) => {
+    const { roomId, callerId } = req.body;
+    if (!roomId || !callerId) {
+        throw new ApiError(400, "RoomId and CallerId are required");
+    }
+    // emit event to notify user that call has ended;
+    req.app.get('io').to(roomId).emit("Call ended", { callerId })
 
-
+    return res
+        .status(200)
+        .message(new ApiResponse(
+            200,
+            { roomId },
+            "Call ended successfully"
+        ))
 });
 
 
@@ -38,7 +50,22 @@ const endCall = asyncHandler(async (req, res) => {
 
 
 
+const getCallHistory = asyncHandler(async (req, res) => {
 
+    const { userId } = req.params;
+    const callLogs = await Call.find({ participants: userId });
+
+    if (!callLogs) {
+        logger
+    }
+    return res
+        .status(200)
+        .message(new ApiResponse(
+            200,
+            { callLogs },
+            "Call History fetched Successfully"
+        ))
+})
 
 
 
@@ -49,5 +76,5 @@ const endCall = asyncHandler(async (req, res) => {
 export {
     initiateCall,
     endCall,
-
+    getCallHistory,
 }
