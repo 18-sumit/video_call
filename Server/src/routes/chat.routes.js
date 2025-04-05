7287f6e17fc57e7158e1dce045d152639706c571
Sumit Singh <sumitsingh3076@gmail.com>
2025-04-05 18:40:24 +0530

    Routes for chat added

--- Server/src/routes/chat.routes.js
+++ Server/src/routes/chat.routes.js
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import { getAllMessages, sendMessage, deleteMessageForSelf } from "../controllers/chat.controller.js";
 
+const chatRouter = Router();
+
+chatRouter.post("/sendmessage", verifyJWT, sendMessage);
+chatRouter.get("/:roomId", verifyJWT, getAllMessages);
+chatRouter.delete("/:messageId", verifyJWT, deleteMessageForSelf)
+
+
+export default chatRouter;
